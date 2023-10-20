7b66fb6c8a17e2fe7bcb59b49f994987ac9b36fc
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-20 10:36:18 -0300

    Adicionada e mapeada a Dto TipoDocumento

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Dtos/Entities/TipoDocumentoDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechVagas_EstagioTech.Dtos.Entities
+{
+	public class TipoDocumentoDto
+	{
+		public int idTipoDocumento { get; set; }
+
+		[Required(ErrorMessage = "Informe a descrição de documento")]
+		[MinLength(3)]
+		[MaxLength(100)]
+		public string? descricaoTipoDocumento { get; set; }
+	}
+}
