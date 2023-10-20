fe6eed3d232b51e6e2cfff42c9a18411b39cc78b
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-20 10:25:52 -0300

    Adicionada e mapeada a Dto cargo

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Model/Entities/CargoModel.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Model/Entities/CargoModel.cs
@@ -1,10 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace TechVagas_EstagioTech.Model.Entities
+﻿namespace TechVagas_EstagioTech.Model.Entities
 {
     public class CargoModel
     {
-        [Key]
         public int CargoId { get; set; }
         public string Descricao { get; set; }
         public string Tipo { get; set; }
