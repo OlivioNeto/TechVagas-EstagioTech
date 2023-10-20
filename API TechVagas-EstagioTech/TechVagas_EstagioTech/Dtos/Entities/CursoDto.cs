b5c929d5b23f40fa06fe613a1c5daee2a2faa11a
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-20 10:31:39 -0300

    Adicionada e mapeada a Dto Curso

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Dtos/Entities/CursoDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechVagas_EstagioTech.Dtos.Entities
+{
+	public class CursoDto
+	{
+		public int idCurso { get; set; }
+
+		[Required(ErrorMessage = "E necessário o nome do curso")]
+		[MinLength(3)]
+		[MaxLength(200)]
+		public string nomeCurso { get; set; }
+	}
+}
