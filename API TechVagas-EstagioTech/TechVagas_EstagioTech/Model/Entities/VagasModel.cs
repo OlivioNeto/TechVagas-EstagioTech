e4c338c48556aa8cdf61e4a52c1227cd30be1360
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-24 17:39:08 -0300

    Criado e definida a VagasModel

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Model/Entities/VagasModel.cs
@@ -0,0 +1,17 @@
+﻿namespace TechVagas_EstagioTech.Model.Entities
+{
+	public class VagasModel
+	{
+		public int VagasId { get; set; }
+		public int Quantidade { get; set; }
+		public DateTime DataPublicacao { get; set; }
+		public DateTime DataLimite { get; set; }
+		public string? Localidade { get; set; }
+		public string? Descricao { get; set; }
+		public string? Titulo { get; set; }
+		public string? LocalidadeTrabalho { get; set; }
+		public string? HorarioEntrada { get; set; }
+		public string? HorarioSaida { get; set; }
+		public string? TotalHorasSemanis { get; set; }
+	}
+}
