e76c600625fe113e137506049c7c1e1c2a3cd87c
Victor Hugo <vhfn2004@gmail.com>
2023-10-25 16:06:21 -0300

    Chamado o metado BuscarTodasVagas na VagasController

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Controllers/VagasController.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Controllers/VagasController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TechVagas_EstagioTech.Dtos.Entities;
+using TechVagas_EstagioTech.Repositorios.Interfaces;
 
 namespace TechVagas_EstagioTech.Controllers
 {
@@ -7,5 +9,19 @@ namespace TechVagas_EstagioTech.Controllers
     [ApiController]
     public class VagasController : ControllerBase
     {
+        private readonly IVagasInteface _vagas;
+
+        public VagasController(IVagasInteface vagas)
+        {
+            _vagas = vagas;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<VagasDto>>> Get()
+        {
+            var vagasDto = await _vagas.BuscarTodasVagas();
+            if (vagasDto == null) return NotFound("Vagas não encontradas!");
+            return Ok(vagasDto);
+        }
     }
 }
