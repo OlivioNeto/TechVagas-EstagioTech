d87b2c5b0965968b96440b121765fec3398bd8f7
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-25 20:51:14 -0300

    Criado a VagasService e IVagasServide e definido a Busca por Id na VagasService

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Entities/VagasService.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using TechVagas_EstagioTech.Dtos.Entities;
+using TechVagas_EstagioTech.Repositorios;
+using TechVagas_EstagioTech.Repositorios.Interfaces;
+
+namespace TechVagas_EstagioTech.Services.Entities
+{
+	public class VagasService
+	{
+		private readonly IVagasRepositorio _vagasRepositorio;
+		private readonly IMapper _mapper;
+
+		public VagasService(IVagasRepositorio vagasRepositorio, IMapper mapper)
+		{
+			_vagasRepositorio = vagasRepositorio;
+			_mapper = mapper;
+		}
+
+		public async Task<VagasDto> BuscarPorId(int id)
+		{
+			var vagas = await _vagasRepositorio.BuscarPorId(id);
+			return _mapper.Map<VagasDto>(vagas);
+		}
+	}
+}
