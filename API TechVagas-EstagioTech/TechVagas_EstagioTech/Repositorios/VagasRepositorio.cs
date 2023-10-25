bb79ae2f580ef7457f1c738beb76801f4428d84c
Victor Hugo <vhfn2004@gmail.com>
2023-10-25 15:44:23 -0300

    Criado a VagasRepositorio e definida a funcao Busca por Id

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Repositorios/VagasRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechVagas_EstagioTech.Data;
+using TechVagas_EstagioTech.Model.Entities;
+
+namespace TechVagas_EstagioTech.Repositorios
+{
+    public class VagasRepositorio
+    {
+        private readonly DBContext _dbContext;
+
+        public VagasRepositorio(DBContext cargoDBContex)
+        {
+            _dbContext = cargoDBContex;
+        }
+        public async Task<VagasModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Vagas.FirstOrDefaultAsync(x => x.VagasId == id);
+        }
+    }
+}
