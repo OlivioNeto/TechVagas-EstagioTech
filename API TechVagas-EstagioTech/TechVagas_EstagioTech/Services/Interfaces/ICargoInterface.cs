51b4d2508976af253c37553a78e193539c325488
Victor Hugo <vhfn2004@gmail.com>
2023-10-25 17:18:56 -0300

    criado a cargo Interface e mapeada

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/ICargoInterface.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/ICargoInterface.cs
@@ -1,6 +1,13 @@
-﻿namespace TechVagas_EstagioTech.Services.Interfaces
+﻿using TechVagas_EstagioTech.Dtos.Entities;
+
+namespace TechVagas_EstagioTech.Services.Interfaces
 {
     public interface ICargoInterface
     {
+        Task<IEnumerable<CargoDto>> BuscarTodosCargos();
+        Task<CargoDto> BuscarPorId(int id);
+        Task Adicionar(CargoDto cargoDto);
+        Task Atualizar(CargoDto cargoDto);
+        Task Apagar(int id);
     }
 }
