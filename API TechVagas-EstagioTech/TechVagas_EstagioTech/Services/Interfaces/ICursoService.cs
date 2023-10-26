d01f0fcb05454bacd265aa1d0a1fcb27adf33986
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-26 10:36:06 -0300

    Adicionado os metados na ICursoService

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/ICursoService.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/ICursoService.cs
@@ -1,6 +1,13 @@
-﻿namespace TechVagas_EstagioTech.Services.Interfaces
+﻿using TechVagas_EstagioTech.Dtos.Entities;
+
+namespace TechVagas_EstagioTech.Services.Interfaces
 {
 	public interface ICursoService
 	{
+		Task<IEnumerable<CursoDto>> BuscarTodosCursos();
+		Task<CursoDto> BuscarPorId(int id);
+		Task Adicionar(CursoDto cursoDto);
+		Task Atualizar(CursoDto cursoDto);
+		Task Apagar(int id);
 	}
 }
