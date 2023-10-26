0a6c53357787c90683b81a37341fbba267f8d10c
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-26 11:14:39 -0300

    Criado a TipoDocumentoService e ITipoDocumentoService

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Entities/TipoDocumentoService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TechVagas_EstagioTech.Repositorios.Interfaces;
+using TechVagas_EstagioTech.Services.Interfaces;
+
+namespace TechVagas_EstagioTech.Services.Entities
+{
+	public class TipoDocumentoService : ITipoDocumentoService
+	{
+		private readonly ITipoDocumentoRepositorio _tipoDocumentoRepositorio;
+		private readonly IMapper _mapper;
+
+		public TipoDocumentoService(ITipoDocumentoRepositorio tipoDocumentoRepositorio, IMapper mapper)
+		{
+			_tipoDocumentoRepositorio = tipoDocumentoRepositorio;
+			_mapper = mapper;
+		}
+	}
+}
