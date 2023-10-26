ed45f8fe84044da8c7d458f79e08fd610eb3141e
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-26 11:22:39 -0300

    Puxado todos metados na ITipoDocumentoService

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/ITipoDocumentoService.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/ITipoDocumentoService.cs
@@ -1,6 +1,13 @@
-﻿namespace TechVagas_EstagioTech.Services.Interfaces
+﻿using TechVagas_EstagioTech.Dtos.Entities;
+
+namespace TechVagas_EstagioTech.Services.Interfaces
 {
 	public interface ITipoDocumentoService
 	{
+		Task<IEnumerable<TipoDocumentoDto>> BuscarTodosTipoDocumentos();
+		Task<TipoDocumentoDto> BuscarPorId(int id);
+		Task Adicionar(TipoDocumentoDto tipoDocumentoDto);
+		Task Atualizar(TipoDocumentoDto tipoDocumentoDto);
+		Task Apagar(int id);
 	}
 }
