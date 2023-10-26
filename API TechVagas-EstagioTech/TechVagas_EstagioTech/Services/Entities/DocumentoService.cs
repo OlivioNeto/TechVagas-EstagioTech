0673ad46242f2430d9aeb06922745dcd2ebdfaa4
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-26 10:59:03 -0300

    Criado a DocumentoService e criada a BuscaPorId

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Entities/DocumentoService.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using TechVagas_EstagioTech.Dtos.Entities;
+using TechVagas_EstagioTech.Repositorios;
+using TechVagas_EstagioTech.Repositorios.Interfaces;
+using TechVagas_EstagioTech.Services.Interfaces;
+
+namespace TechVagas_EstagioTech.Services.Entities
+{
+	public class DocumentoService : IDocumentoService
+	{
+		private readonly IDocumentoRepositorio _documentoRepositorio;
+		private readonly IMapper _mapper;
+
+		public DocumentoService(IDocumentoRepositorio documentoRepositorio, IMapper mapper)
+		{
+			_documentoRepositorio = documentoRepositorio;
+			_mapper = mapper;
+		}
+
+		public async Task<DocumentoDto> BuscarPorId(int id)
+		{
+			var documento = await _documentoRepositorio.BuscarPorId(id);
+			return _mapper.Map<DocumentoDto>(documento);
+		}
+	}
+}
