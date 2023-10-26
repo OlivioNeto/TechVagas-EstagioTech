473dbdada726929258152d4784c6015d8d95e663
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-26 10:24:26 -0300

    Criado a Busca por Id na CursoService

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Entities/CursoService.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Entities/CursoService.cs
@@ -1,11 +1,28 @@
 ﻿using AutoMapper;
+using TechVagas_EstagioTech.Dtos.Entities;
+using TechVagas_EstagioTech.Repositorios;
 using TechVagas_EstagioTech.Repositorios.Interfaces;
+using TechVagas_EstagioTech.Services.Interfaces;
 
 namespace TechVagas_EstagioTech.Services.Entities
 {
-	public class CursoService
+	public class CursoService : ICursoService
 	{
 		private readonly ICursoRepositorio _cursoRepositorio;
 		private readonly IMapper _mapper;
+
+		public CursoService(ICursoRepositorio cursoRepositorio, IMapper mapper)
+		{
+			_cursoRepositorio = cursoRepositorio;
+			_mapper = mapper;
+		}
+
+		public async Task<CursoDto> BuscarPorId(int id)
+		{
+			var curso = await _cursoRepositorio.BuscarPorId(id);
+			return _mapper.Map<CursoDto>(curso);
+		}
+
+
 	}
 }
