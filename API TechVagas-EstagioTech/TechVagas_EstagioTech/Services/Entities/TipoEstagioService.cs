553de693c084073f19ef1b7010dca0cc56104f4d
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-26 22:05:47 -0300

    Criado a funcao BuscaPorId na TipoEstagio Service

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Entities/TipoEstagioService.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using TechVagas_EstagioTech.Dtos.Entities;
+using TechVagas_EstagioTech.Repositorios;
+using TechVagas_EstagioTech.Repositorios.Interfaces;
+using TechVagas_EstagioTech.Services.Interfaces;
+
+namespace TechVagas_EstagioTech.Services.Entities
+{
+	public class TipoEstagioService : ITipoEstagioService
+	{
+		private readonly ITipoEstagioRepositorio _tipoEstagioRepositorio;
+		private readonly IMapper _mapper;
+
+		public TipoEstagioService(ITipoEstagioRepositorio tipoEstagioRepositorio, IMapper mapper)
+		{
+			_tipoEstagioRepositorio = tipoEstagioRepositorio;
+			_mapper = mapper;
+		}
+
+		public async Task<TipoEstagioDto> BuscarPorId(int id)
+		{
+			var tipoEstagio = await _tipoEstagioRepositorio.BuscarPorId(id);
+			return _mapper.Map<TipoEstagioDto>(tipoEstagio);
+		}
+	}
+}
