0cc9b87fc94b2f2e2437ae1bf377d2d8683837c6
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-10-26 22:12:20 -0300

    Mapeada a Interface de TipoEstagioService

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/ITipoEstagioService.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/ITipoEstagioService.cs
@@ -1,6 +1,13 @@
-﻿namespace TechVagas_EstagioTech.Services.Interfaces
+﻿using TechVagas_EstagioTech.Dtos.Entities;
+
+namespace TechVagas_EstagioTech.Services.Interfaces
 {
 	public interface ITipoEstagioService
 	{
+		Task<IEnumerable<TipoEstagioDto>> BuscarTodosTipoEstagio();
+		Task<TipoEstagioDto> BuscarPorId(int id);
+		Task Adicionar(TipoEstagioDto tipoEstagioDto);
+		Task Atualizar(TipoEstagioDto tipoEstagioDto);
+		Task Apagar(int id);
 	}
 }
