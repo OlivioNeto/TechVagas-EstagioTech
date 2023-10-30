15779a23ca02d370cfd3a2ba89938c45a6a824c1
Victor Hugo <vhfn2004@gmail.com>
2023-10-30 13:53:04 -0300

    Criado a IConcedenteRepositorio e chamada as funções

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Repositorios/Interfaces/IConcedenteRepositorio.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Repositorios/Interfaces/IConcedenteRepositorio.cs
@@ -1,6 +1,17 @@
-﻿namespace TechVagas_EstagioTech.Repositorios.Interfaces
+﻿using TechVagas_EstagioTech.Model.Entities;
+
+namespace TechVagas_EstagioTech.Repositorios.Interfaces
 {
     public interface IConcedenteRepositorio
     {
+        Task<List<ConcedenteModel>> BuscarTodosConcedentes();
+
+        Task<ConcedenteModel> BuscarPorId(int id);
+
+        Task<ConcedenteModel> Adicionar(ConcedenteModel concedenteModel);
+
+        Task<ConcedenteModel> Atualizar(ConcedenteModel concedenteModel);
+
+        Task<bool> Apagar(int id);
     }
 }
