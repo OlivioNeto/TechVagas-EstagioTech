02290e5467d4fcc65780ba115943bb06c740a5cb
Victor Hugo <vhfn2004@gmail.com>
2023-10-30 13:23:13 -0300

    criado a ConcedenteModel e ja adicionado as classes

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Model/Entities/ConcedenteModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechVagas_EstagioTech.Model.Entities
+{
+    [Table("concedente")]
+    public class ConcedenteModel
+    {
+        [Column("concedenteid")]
+        public int concedenteId { get; set; }
+
+
+        [Column("razaosocial")]
+        public string? RazaoSocial { get; set; }
+
+
+        [Column("responsavelestagio")]
+        public string? ResponsavelEstagio { get; set;}
+
+
+        [Column("cnpj")]
+        public string? Cnpj { get; set; }
+
+
+        [Column("localidade")]
+        public string? Localidade { get; set; }
+    }
+}
