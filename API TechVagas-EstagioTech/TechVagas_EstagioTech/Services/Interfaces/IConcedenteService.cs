a09849385bbfba0779ab6cf3face76873d05497d
Victor Hugo <vhfn2004@gmail.com>
2023-10-30 14:14:19 -0300

    Criado a IConcedenteService e chamada todas funcoes

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/IConcedenteService.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/IConcedenteService.cs
@@ -1,6 +1,13 @@
-﻿namespace TechVagas_EstagioTech.Services.Interfaces
+﻿using TechVagas_EstagioTech.Dtos.Entities;
+
+namespace TechVagas_EstagioTech.Services.Interfaces
 {
     public interface IConcedenteService
     {
+        Task<IEnumerable<ConcedenteDto>> BuscarTodosConcedentes();
+        Task<ConcedenteDto> BuscarPorId(int id);
+        Task Adicionar(ConcedenteDto concedenteDto);
+        Task Atualizar(ConcedenteDto concedenteDto);
+        Task Apagar(int id);
     }
 }
