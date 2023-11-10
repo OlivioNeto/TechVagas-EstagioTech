d24b23a258615fa6d7915fb478bed602844559db
Victor Hugo <vhfn2004@gmail.com>
2023-11-10 14:09:45 -0300

    Criada a IAlunosRepositorio e chamada as funções

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Repositorios/Interfaces/IAlunoRepositorio.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Repositorios/Interfaces/IAlunoRepositorio.cs
@@ -1,6 +1,17 @@
-﻿namespace TechVagas_EstagioTech.Repositorios.Interfaces
+﻿using TechVagas_EstagioTech.Model.Entities;
+
+namespace TechVagas_EstagioTech.Repositorios.Interfaces
 {
     public interface IAlunoRepositorio
     {
+        Task<List<AlunoModel>> BuscarTodosAlunos();
+
+        Task<AlunoModel> BuscarPorId(int id);
+
+        Task<AlunoModel> Adicionar(AlunoModel alunoModel);
+
+        Task<AlunoModel> Atualizar(AlunoModel alunoModel);
+
+        Task<bool> Apagar(int id);
     }
 }
