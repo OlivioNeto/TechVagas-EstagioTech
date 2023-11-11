75ed239484a7f89e5693119c3290f9dd65f42afd
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-11-10 23:01:10 -0300

    Criado a IAlunoService e puxada as funções

--- API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/IAlunoService.cs
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Services/Interfaces/IAlunoService.cs
@@ -1,6 +1,13 @@
-﻿namespace TechVagas_EstagioTech.Services.Interfaces
+﻿using TechVagas_EstagioTech.Dtos.Entities;
+
+namespace TechVagas_EstagioTech.Services.Interfaces
 {
 	public interface IAlunoService
 	{
+		Task<IEnumerable<AlunoDto>> BuscarTodosAlunos();
+		Task<AlunoDto> BuscarPorId(int id);
+		Task Adicionar(AlunoDto alunoDto);
+		Task Atualizar(AlunoDto alunoDto);
+		Task Apagar(int id);
 	}
 }
