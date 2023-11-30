790fcf6cb14ad4440a55af60deafc9e88feb26cf
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-11-29 22:35:05 -0300

    :sparkles: Adicionado a Usuario, TipoUsuario e Login na Model

---
+++ API TechVagas-EstagioTech/TechVagas_EstagioTech/Model/Entities/TipoUsuarioModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechVagas_EstagioTech.Model.Entities
+{
+    [Table("tipousuario")]
+    public class TipoUsuarioModel
+    {
+        [Column("idtipousuario")]
+        public int TipoUsuarioId { get; set; }
+
+        [Column("nivelacesso")]
+        public string NivelAcesso { get; set; }
+
+        [Column("nometipousuario")]
+        public string NomeTipoUsuario { get; set; }
+
+        [Column("descricaotipousuario")]
+        public string DescricaoTipoUsuario { get; set; }
+
+        public ICollection<UsuarioModel>? Usuarios { get; set; }
+
+    }
+}
