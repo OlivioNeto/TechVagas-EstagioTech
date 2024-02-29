90a5e9abf2d6036d3fb4b471797ffce6a313e6c2
Victor Hugo <vhfn2004@gmail.com>
2024-02-29 12:58:41 -0300

    criado a model InstituicaoEnsino e definida os campos

---
+++ Desenvolvimento/API TechVagas-EstagioTech/TechVagas_EstagioTech/Model/Entities/InstituicaoEnsinoModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechVagas_EstagioTech.Model.Entities
+{
+    [Table("instituicaoensino")]
+    public class InstituicaoEnsinoModel
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("nomeinstituicao")]
+        public string NomeInstituicao { get; set; }
+
+        [Column("local")]
+        public string Local {  get; set; }
+
+        [Column("telefone")]
+        public string Telefone { get; set; }
+
+    }
+}
