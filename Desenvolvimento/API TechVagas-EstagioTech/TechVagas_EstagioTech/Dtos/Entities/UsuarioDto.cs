82550f72f2c29d85e7d5ba34fdffa3e241171635
Victor Hugo <vhfn2004@gmail.com>
2024-04-04 13:27:31 -0300

    criado a Model e a Dto Usuario

---
+++ Desenvolvimento/API TechVagas-EstagioTech/TechVagas_EstagioTech/Dtos/Entities/UsuarioDto.cs
@@ -0,0 +1,18 @@
+﻿namespace TechVagas_EstagioTech.Dtos.Entities
+{
+    public class UsuarioDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public UserType Type { get; set; }
+    }
+
+    public enum UserType
+    {
+        Administrador,
+        Aluno,
+        Coordenador,
+        Empresa
+    }
+}
---
+++ Desenvolvimento/API TechVagas-EstagioTech/TechVagas_EstagioTech/Model/Entities/UsuarioModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechVagas_EstagioTech.Model.Entities
+{
+    [Table("usuario")]
+    public class UsuarioModel
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("senha")]
+        public string Senha { get; set; }
+        public UserType Type { get; set; }
+    }
+
+    public enum UserType
+    {
+        Administrador,
+        Aluno,
+        Coordenador,
+        Empresa
+    }
+}
+
