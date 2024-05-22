933d3f32b846df250b61a668d6b0ede305be6a73
Victor Hugo <vhfn2004@gmail.com>
2024-05-22 16:59:28 -0300

    feita a sessao dto

--- Desenvolvimento/API TechVagas-EstagioTech/TechVagas_EstagioTech/Dtos/Entities/EntitySecurity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TechVagas_EstagioTech.Dtos.Entities
-{
-    public class EntitySecurity
-    {
-        public string Issuer { get; } = "TECHVAGAS_Client";
-        public string Audience { get; } = "TECHVAGAS_Server";
-        public string Key { get; } = "TECHVAGAS_System";
-    }
-}
