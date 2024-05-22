53ba3097c8d62edcaad003417a5059d8738fbdc1
Victor Hugo <vhfn2004@gmail.com>
2024-05-22 17:10:51 -0300

    feita a sessao repositorio

---
+++ Desenvolvimento/API TechVagas-EstagioTech/TechVagas_EstagioTech/Repositorios/Interfaces/ISessaoRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using TechVagas_EstagioTech.Model.Entities;
+
+namespace TechVagas_EstagioTech.Repositorios.Interfaces
+{
+    public interface ISessaoRepositorio
+    {
+        Task<IEnumerable<SessaoModel>> GetAll();
+        Task<IEnumerable<IEnumerable<SessaoModel>>> GetAllSessionsGroupedByUser();
+        Task<IEnumerable<SessaoModel>> GetOpenSessions();
+        Task<IEnumerable<SessaoModel>> GetCloseSessions();
+        Task<SessaoModel> GetLastSession(int id);
+        Task<SessaoModel> GetById(int id);
+        Task<SessaoModel> GetByToken(string token);
+        Task<UsuarioModel> GetUser(string token);
+        Task<SessaoModel> Create(SessaoModel sessaoModel);
+        Task<SessaoModel> Update(SessaoModel sessaoModel);
+        Task<SessaoModel> Delete(int id);
+
+        Task<IEnumerable<UsuarioModel>> GetOnlineUsers();
+        Task<IEnumerable<UsuarioModel>> GetOfflineUsers();
+        Task<IEnumerable<SessaoModel>> GetOpenSessionByUser(int id);
+    }
+}
